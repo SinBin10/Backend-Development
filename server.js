f2f818d1a68c8567b846dce1b73574a7bf3e09cd
SinBin10 <bincode10@gmail.com>
2024-07-25 18:03:24 +0530

    Error handling using express.

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 //A middleware interferes with a request before it reaches the routes
-//the below is an example of a middleware
+//the below is an example of a middleware, there can be multiple middlewares
 app.use(function (req, res, next) {
   console.log("This is a middleware..");
+  next();
 });
 
 app.get("/", function (req, res) {
@@ -12,7 +13,12 @@ app.get("/", function (req, res) {
 });
 
 app.get("/check", function (req, res) {
-  res.send("running server with nodemon...");
+  return next(new Error("Someting went wrong !"));
+});
+
+app.use((err, req, res, next) => {
+  //   console.log("An error");
+  res.status(500).send("Error occured !");
 });
 
 app.listen(3000);
